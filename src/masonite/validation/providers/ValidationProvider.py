f9105375f407e28e4c642755aa8a122ff9f9ba6c
Joseph Mancuso <idmann509@gmail.com>
2019-06-02 11:03:07 -0400

    added masonite commands

--- src/masonite/validation/providers/ValidationProvider.py
+++ src/masonite/validation/providers/ValidationProvider.py
@@ -2,7 +2,8 @@
 
 from masonite.provider import ServiceProvider
 from .. import Validator, ValidationFactory
-
+from ..commands.RuleEnclosureCommand import RuleEnclosureCommand
+from ..commands.RuleCommand import RuleCommand
 
 class ValidationProvider(ServiceProvider):
 
@@ -10,6 +11,8 @@ class ValidationProvider(ServiceProvider):
 
     def register(self):
         self.app.singleton('Validator', Validator)
+        self.app.simple(RuleEnclosureCommand())
+        self.app.simple(RuleCommand())
 
     def boot(self, validator: Validator):
         validator.extend(ValidationFactory().registry)
