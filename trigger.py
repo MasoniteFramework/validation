1d09fa7b4c3f1894b387a907da6f9a579ef79dbb
Joseph Mancuso <idmann509@gmail.com>
2019-07-05 22:26:32 -0400

    added trigger

---
+++ trigger.py
@@ -0,0 +1,17 @@
+import requests
+import time
+import os
+
+r = requests.post('https://circleci.com/api/v1/project/masoniteframework/validation/tree/circle-ci?circle-token={}'.format('CIRCLE_TOKEN'))
+print(r.json()['build_num'])
+
+status = requests.get('https://circleci.com/api/v1.1/project/github/masoniteframework/validation/{}?circle-token={}'.format(r.json()['build_num'], os.getenv('CIRCLE_TOKEN')))
+print('getting ... status', status.json()['lifecycle'])
+while status.json()['lifecycle'] != 'finished':
+    print('lopping ... status', status.json()['lifecycle'])
+    time.sleep(1)
+    status = requests.get('https://circleci.com/api/v1.1/project/github/masoniteframework/validation/{}?circle-token={}'.format(r.json()['build_num'], os.getenv('CIRCLE_TOKEN')))
+
+print('up, finshed with status. Failed?', status.json()['failed'])
+if status.json()['failed']:
+    exit(1)
