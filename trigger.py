2c2ab5897e61ef59480ef80809b5db933fd219b0
Joseph Mancuso <idmann509@gmail.com>
2019-07-05 22:34:28 -0400

    fixed trigger

--- trigger.py
+++ trigger.py
@@ -2,7 +2,7 @@ import requests
 import time
 import os
 
-r = requests.post('https://circleci.com/api/v1/project/masoniteframework/validation/tree/circle-ci?circle-token={}'.format('CIRCLE_TOKEN'))
+r = requests.post('https://circleci.com/api/v1/project/masoniteframework/validation/tree/circle-ci?circle-token={}'.format(os.getenv('CIRCLE_TOKEN')))
 print(r.json()['build_num'])
 
 status = requests.get('https://circleci.com/api/v1.1/project/github/masoniteframework/validation/{}?circle-token={}'.format(r.json()['build_num'], os.getenv('CIRCLE_TOKEN')))
