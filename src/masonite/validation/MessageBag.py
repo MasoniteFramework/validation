148d4e85e08e06d4137589ede9170725c7ecee97
Joseph Mancuso <idmann509@gmail.com>
2019-09-14 23:36:15 -0400

    added message bag

---
+++ src/masonite/validation/MessageBag.py
@@ -0,0 +1,72 @@
+class MessageBag:
+
+    items = {}
+
+    def add(self, error, message):
+        """Adds an error and message to the message bag
+
+        Arguments:
+            error {string} -- The error to add
+            message {string} -- The message to add
+        """
+
+        if error in self.items:
+            self.items[error].append(message)
+        else:
+            self.items.update({error: [message]})
+
+    def all(self):
+        """Get all errors and messages
+        """
+        return self.items
+
+    def any(self):
+        """If the messagebag has any errors
+        """
+        return len(self.items) > 0
+
+    def first(self):
+        """Gets the first error and message
+        """
+        for key, value in self.items.items():
+            return {key: value}
+
+    def count(self):
+        """Gets the amount of errors
+        """
+        return len(self.items)
+
+    def amount(self, key):
+        """Gets the amount of messages
+        
+        Arguments:
+            key {string} -- the error to get the amount of.
+        
+        Returns:
+            int -- Returns the amount of messages
+        """
+        return len(self.items[key])
+
+    def get(self, key):
+        """Gets all the messages for a specific error
+        """
+        return self.items[key]
+
+    def errors(self):
+        """Gets a list of errors
+        """
+        return list(self.items.keys())
+
+    def messages(self):
+        """Gets a list of all the messages
+        """
+        messages = []
+        for error, message in self.items.items():
+            messages += message
+        
+        return messages
+
+    def reset(self):
+        """Gets a list of all the messages
+        """
+        self.items = {}
---
+++ tests/test_messagebag.py
@@ -0,0 +1,65 @@
+import unittest
+from src.masonite.validation import MessageBag
+
+class TestMessageBag(unittest.TestCase):
+
+    def setUp(self):
+        self.bag = MessageBag()
+
+    def test_message_bag_can_add_errors_and_messages(self):
+        self.bag.add('email', 'Your email is invalid')
+        self.assertEqual(self.bag.items, {'email': ['Your email is invalid']})
+
+    def test_message_bag_can_add_several_errors_and_messages(self):
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('email', 'Your email is invalid')
+
+    def test_message_bag_can_get_all_errors_and_messages(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.assertEqual(self.bag.all(), {'email': ['Your email is invalid']})
+
+    def test_message_bag_has_any_errors(self):
+        self.bag.reset()
+        self.assertFalse(self.bag.any())
+        self.bag.add('email', 'Your email is invalid')
+        self.assertTrue(self.bag.any())
+
+    def test_message_bag_can_get_first_error(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('username', 'Your username is invalid')
+        self.assertEqual(self.bag.first(), {'email': ['Your email is invalid']})
+
+    def test_amount_of_messages(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('email', 'Your email too short')
+        self.assertEqual(self.bag.amount('email'), 2)
+
+    def test_get_messages(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('email', 'Your email too short')
+        self.assertEqual(
+            self.bag.get('email'), 
+            ['Your email is invalid', 'Your email too short']
+        )
+
+    def test_get_errors(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('email', 'Your email too short')
+        self.assertEqual(
+            self.bag.errors(), 
+            ['email']
+        )
+
+    def test_get_messages(self):
+        self.bag.reset()
+        self.bag.add('email', 'Your email is invalid')
+        self.bag.add('username', 'Your username too short')
+        self.assertEqual(
+            self.bag.messages(), 
+            ['Your email is invalid', 'Your username too short']
+        )
