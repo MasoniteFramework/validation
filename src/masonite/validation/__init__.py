1bccc9e7ef1bba94c79d68c52c59a7f5cc3ae8e5
Samuel Girardin <samuelgirardin@gmail.com>
2020-08-08 15:41:04 +0200

    add regex validator to import validator list

--- src/masonite/validation/__init__.py
+++ src/masonite/validation/__init__.py
@@ -5,4 +5,4 @@ from .Validator import (BaseValidation, ValidationFactory, Validator, accepted,
                         date, does_not, email, equals, exists, greater_than, in_range,
                         ip, is_future, is_in, is_past, isnt, json, length,
                         less_than, none, numeric, phone, required, string, strong,
-                        timezone, truthy, when)
+                        timezone, truthy, when, regex)
