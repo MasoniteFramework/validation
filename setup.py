e5e56c6db8d4d369075fef4f806e91092cf0d8bb
Joe Mancuso <idmann509@gmail.com>
2020-09-17 21:13:33 -0400

    added postal coded validation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     ],
     package_dir = {'': 'src'},
     include_package_data=True,
-    version='3.0.9',
+    version='3.0.10',
     install_requires=[
         'masonite-dot>=0.0.5',
         'hfilesize==0.1.0'
